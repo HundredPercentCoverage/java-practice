b69cb35a2b9fb80167364637d39a35c3793cc826
bunglenutter <bunglenutter@users.noreply.github.com>
2017-06-20 00:23:58 +0100

    Add files via upload

--- javafordummies/DisplayPlaces/DisplayThePlaces.java
+++ javafordummies/DisplayPlaces/DisplayThePlaces.java
@@ -1,46 +1,32 @@
 import java.util.Scanner;
 import static java.lang.System.out;
+import java.io.File;
+import java.io.IOException;
 
 public class DisplayThePlaces {
 
-    public static void main(String[] args) {
-	PlaceToLive one, two, three;
+    public static void main(String[] args) throws IOException {
+	Scanner diskScanner = new Scanner(new File("Places.txt"));
 	
-	one = new PlaceToLive();
-	one.setAddress("5 Carrickburn Road");
-	one.setBedrooms(4);
-	one.setArea(100.67);
+	for(int i = 0; i < 3; i++){
+	    display(diskScanner);
+	}
 	
-	two = new PlaceToLive();
-	two.setAddress("22 Lisbane Gardens");
-	two.setBedrooms(3);
-	two.setArea(54.76);
-	
-	three = new PlaceToLive();
-	three.setAddress("9 Jordanstown Mews");
-	three.setBedrooms(2);
-	three.setArea(32.65);
-	
-	display(one);
-	display(two);
-	display(three);
-	
+	diskScanner.close();
     }
     
-    static void display(PlaceToLive place) {
-	Scanner keyboard = new Scanner(System.in);
+    static void display(Scanner aScanner) {
+	PlaceToLive place = new PlaceToLive();
+	
+	place.setAddress(aScanner.nextLine());
+	place.setArea(aScanner.nextDouble());
+	aScanner.nextLine();
+	place.setBedrooms(aScanner.nextInt());
+	aScanner.nextLine();
 	
 	out.println("Address: " + place.getAddress());
-	out.println("Number of bedrooms: " + place.getBedrooms());
-	out.println("Area in square metres: " + place.getArea());
-	
-	out.print("Please enter the cost for the property: £");
-	double totalCost = keyboard.nextDouble();
-	
-	double perSqM = totalCost / place.getArea();
-	double perRoom = totalCost / place.getBedrooms();
-	
-	out.printf("The cost per square metre is: £%,.2f\n", perSqM);
-	out.printf("The cost per room is: £%,.2f\n\n", perRoom);
+	out.printf("Number of bedrooms: %d\n", place.getBedrooms());
+	out.printf("Area: %.2f\n", place.getArea());
     }
+    
 }
