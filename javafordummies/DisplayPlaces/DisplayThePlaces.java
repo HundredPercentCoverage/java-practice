e34073c78fc79b9a1e87b385e4dc73ae63d5bfcf
bunglenutter <bunglenutter@users.noreply.github.com>
2017-06-20 00:26:11 +0100

    Update DisplayThePlaces.java

--- javafordummies/DisplayPlaces/DisplayThePlaces.java
+++ javafordummies/DisplayPlaces/DisplayThePlaces.java
@@ -6,25 +6,25 @@ import java.io.IOException;
 public class DisplayThePlaces {
 
     public static void main(String[] args) throws IOException {
-	Scanner diskScanner = new Scanner(new File("Places.txt"));
+	Scanner diskScanner = new Scanner(new File("Places.txt")); 	//Opens a file called Places.txt
 	
-	for(int i = 0; i < 3; i++){
-	    display(diskScanner);
+	for(int i = 0; i < 3; i++){					//Assume 3 items in file
+	    display(diskScanner);					//Show info from each
 	}
 	
-	diskScanner.close();
+	diskScanner.close();						//Cleanup
     }
     
     static void display(Scanner aScanner) {
-	PlaceToLive place = new PlaceToLive();
+	PlaceToLive place = new PlaceToLive();				//New object to hold info
 	
-	place.setAddress(aScanner.nextLine());
+	place.setAddress(aScanner.nextLine());				//Get relevant info from file
 	place.setArea(aScanner.nextDouble());
 	aScanner.nextLine();
 	place.setBedrooms(aScanner.nextInt());
 	aScanner.nextLine();
 	
-	out.println("Address: " + place.getAddress());
+	out.println("Address: " + place.getAddress());			//Display the info
 	out.printf("Number of bedrooms: %d\n", place.getBedrooms());
 	out.printf("Area: %.2f\n", place.getArea());
     }
